0d5c6405cacf6b7209a57851cc678e4d0163f9b4
kevinzhao <hecheng.zhao@outlook.com>
2019-04-06 01:30:46 -0700

    noises

---
+++ gen_sys_noises.m
@@ -0,0 +1,29 @@
+global_vars;
+
+global cyc_total io_dir;
+
+gen_new = 0; % generate new noises vs. use noises from data file
+
+global sdv_state; % standard deviation of state noises
+sdv_state = [0.01 0.01 0.01]';
+global noise_state; % noises over time on system states
+if gen_new == 1
+    noise_state = ...
+        normrnd(zeros(3, cyc_total), repmat(sdv_state, 1, cyc_total));
+    save([io_dir '/noise_state.mat'], 'noise_state');
+else
+    noise_state = load([io_dir '/noise_state.mat']);
+    noise_state = noise_state.noise_state;
+end
+
+global sdv_msu; % standard deviation of measurement noises
+sdv_msu = [0.01 0.01 0.01 0.01 0.01]';
+global noise_msu; % noises over time on measurements
+if gen_new == 1
+    noise_msu = ...
+        normrnd(zeros(5, cyc_total), repmat(sdv_msu, 1, cyc_total));
+    save([io_dir '/noise_msu.mat'], 'noise_msu');
+else
+    noise_msu = load([io_dir '/noise_msu.mat']);
+    noise_msu = noise_msu.noise_msu;
+end
---
+++ global_vars.m
@@ -0,0 +1,25 @@
+clear all;
+
+global sim_ts; % simulation time step
+sim_ts = 0.0001; 
+
+global sim_total; % total simulation time
+sim_total = 1.0;
+
+global sim_t; % simulation time
+sim_t = 0;
+
+global sim_cyc; % simulation cycle
+sim_cyc = 0;
+
+global cyc_total; % number of total simulation cycles
+cyc_total = sim_total / sim_ts;
+
+global io_dir;
+io_dir = '../phm_io';
+
+global t_seq;
+t_seq = zeros(1, cyc_total);
+for cyc = 1 : cyc_total;
+    t_seq(cyc) = sim_ts * cyc;
+end
