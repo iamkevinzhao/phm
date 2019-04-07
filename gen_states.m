5fb55c3740570d39265b72691c9bf500ab543bac
kevinzhao <hecheng.zhao@outlook.com>
2019-04-07 11:23:20 -0700

    anonymous A matrix

--- gen_states.m
+++ gen_states.m
@@ -12,24 +12,24 @@ global sys_ut sim_ts;
 model_xt = zeros(5, cyc_total);
 sim_xt = zeros(5, cyc_total);
 
+A = @(theta) ...
+    [       -Rs/L1                          0                         0                 (lambda_p*fa(theta))/J    0;
+               0                         -Rs/L1                       0                 (lambda_p*fb(theta))/J    0;
+               0                            0                      -Rs/L1               (lambda_p*fc(theta))/J    0;
+     (lambda_p*fa(theta))/J     (lambda_p*fb(theta))/J      (lambda_p*fc(theta))/J                -B/J            0;
+               0                            0                         0                            P/2            0];    
+           
+B_mat = ...
+    [1/L1     0       0        0;
+      0      1/L1     0        0;
+      0       0      1/L1      0;
+      0       0       0      -1/J;
+      0       0       0        0];    
+
 for cyc = 2 : cyc_total;
     theta = model_xt(5, cyc - 1);
-
-    A = ...
-        [       -Rs/L1                          0                         0                 (lambda_p*fa(theta))/J    0;
-                   0                         -Rs/L1                       0                 (lambda_p*fb(theta))/J    0;
-                   0                            0                      -Rs/L1               (lambda_p*fc(theta))/J    0;
-         (lambda_p*fa(theta))/J     (lambda_p*fb(theta))/J      (lambda_p*fc(theta))/J                -B/J            0;
-                   0                            0                         0                            P/2            0];
-          
-    B_mat = ...
-        [1/L1     0       0        0;
-          0      1/L1     0        0;
-          0       0      1/L1      0;
-          0       0       0      -1/J;
-          0       0       0        0];
     xt = model_xt(:, cyc - 1);
-    x_dot = A * xt + B_mat * sys_ut(:, cyc);
+    x_dot = A(theta) * xt + B_mat * sys_ut(:, cyc);
     model_xt(:, cyc) = xt + x_dot .* sim_ts;
 end
 
